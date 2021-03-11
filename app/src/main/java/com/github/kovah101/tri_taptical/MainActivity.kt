509e1fe08b314a1461fd59153c9c6a38be4fc649
Elliot Thompson <elliotthompson101@gmail.com>
2021-03-11 12:13:02 +0000

    Setup Project

---
+++ app/src/main/java/com/github/kovah101/tri_taptical/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.kovah101.tri_taptical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
