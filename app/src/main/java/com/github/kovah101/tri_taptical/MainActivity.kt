08f549adfc2b8e0d3c4c8de98093d4b7ff8d1baf
Elliot Thompson <elliotthompson101@gmail.com>
2021-03-12 11:43:08 +0000

    Starting Button Click function

--- app/src/main/java/com/github/kovah101/tri_taptical/MainActivity.kt
+++ app/src/main/java/com/github/kovah101/tri_taptical/MainActivity.kt
@@ -2,10 +2,28 @@ package com.github.kovah101.tri_taptical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeColor(view: View) {
+            val selectedButton = view as Button
+            var cellID = ""
+        when(selectedButton.id){
+            R.id.topLeft -> cellID = "topLeft"
+            R.id.topMiddle -> cellID = "topMiddle"
+            R.id.topRight -> cellID = "topRight"
+        }
+        Log.d("ButtonPress","selected button: ${selectedButton.id}")
+    }
+
+    fun confirmMove(view: View) {
+
     }
 }
