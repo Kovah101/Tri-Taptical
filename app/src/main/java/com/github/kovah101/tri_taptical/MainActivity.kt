2890ce1ec082e8a59ff433795806742b14d50af3
Elliot Thompson <elliotthompson101@gmail.com>
2021-03-15 20:59:52 +0000

    Confirms winner, need to show winner

--- app/src/main/java/com/github/kovah101/tri_taptical/MainActivity.kt
+++ app/src/main/java/com/github/kovah101/tri_taptical/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,12 +25,16 @@ class MainActivity : AppCompatActivity() {
     var oldCellID = -1
     var trueCellID = -1
     var activePlayer = 1
+    //var activePlayerColor = 0
     var confirmedMoves = HashMap<Int, Int>()
+    var winningMoves = arrayListOf<Int>()
     var selectedCell = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // activePlayerColor = resources.getColor(R.color.playerOne)
     }
 
     fun changeColor(view: View) {
@@ -144,17 +149,150 @@ class MainActivity : AppCompatActivity() {
         oldCellID = -1
         // reset true cell ID
         trueCellID = -1
-        // change player
+        // check for winner
         checkForWinner()
-        activePlayer = if (activePlayer == 1) {
-            2
+
+        // change player
+        if (activePlayer == 1) {
+            activePlayer = 2
+            //activePlayerColor = resources.getColor(R.color.playerTwo)
         } else {
-            1
+            activePlayer = 1
+            //activePlayerColor = resources.getColor(R.color.playerOne)
         }
     }
 
     // TODO finish function
-    fun checkForWinner(){
+    private fun checkForWinner() {
+        var winFlag = false
 
+        // check for Spot winner
+        if (spotWinner()) {
+            winFlag = true
+            Log.d("ButtonPress", "We have a spot winner")
+        }
+        // check for horizontal winner
+        if (horizontalWinner()) {
+            winFlag = true
+            Log.d("ButtonPress", "We have a horizontal winner")
+        }
+        //check for vertical winner
+        if (verticalWinner()) {
+            winFlag = true
+            Log.d("ButtonPress", "We have a vertical winner")
+        }
+        //check for diagonal winner
+        if (diagonalWinner()) {
+            winFlag = true
+            Log.d("ButtonPress", "We have a diagonal winner")
+        }
+
+
+        // if there is a winner, add to scoreboard, show toast notification & reset board
+        if (winFlag) {
+            // increment score counter
+
+            // show toast win notification
+            Toast.makeText(this, "Player $activePlayer is the Winner!", Toast.LENGTH_LONG).show()
+
+            // reset the board
+            val white = resources.getColor(R.color.white)
+            for (cell in locationIDs.indices) {
+                val currentCell = findViewById<View>(locationIDs[cell])
+                currentCell.setBackgroundColor(white)
+            }
+            // show winning moves
+            // TODO colour winning moves, wait then clear
+            for (move in winningMoves){
+                println("winning move: $move")
+//                val winningCell = findViewById<View>(move)
+//                winningCell.setBackgroundColor(R.color.playerOne)
+            } // wait 2s
+//            Thread.sleep(2000)
+//            // clear winning moves
+//            for (move in winningMoves){
+//                val winningCell = findViewById<View>(move)
+//                winningCell.setBackgroundColor(white)
+//            }
+            // clear saved moves
+            confirmedMoves.clear()
+            winningMoves.clear()
+
+        }
+    }
+
+    private fun spotWinner(): Boolean {
+        for (i in 0..locationIDs.size step 3) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[i + 1] == activePlayer && confirmedMoves[i + 2] == activePlayer) {
+                winningMoves.add(i)
+                winningMoves.add(i + 1)
+                winningMoves.add(i + 2)
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun horizontalWinner(): Boolean {
+        // check for same size winning rows
+        for (i in 0..18 step 9) {
+            for (j in 0..2) {
+                var k = i + j
+                if (confirmedMoves[k] == activePlayer && confirmedMoves[k + 3] == activePlayer && confirmedMoves[k + 6] == activePlayer) {
+                    return true
+                }
+            }
+        }
+        // check ascending size
+        for (i in 2..20 step 9) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[i + 2] == activePlayer && confirmedMoves[i + 4] == activePlayer) {
+                return true
+            }
+        }
+        // check descending size
+        for (i in 0..18 step 9) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[i + 4] == activePlayer && confirmedMoves[i + 8] == activePlayer) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun verticalWinner(): Boolean {
+        // check for same size winning columns
+        for (i in 0..8) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[i + 9] == activePlayer && confirmedMoves[i + 18] == activePlayer) {
+                return true
+            }
+        }
+        // check ascending size
+        for (i in 2..8 step 3) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[i + 8] == activePlayer && confirmedMoves[i + 16] == activePlayer) {
+                return true
+            }
+        }
+        // check descending size
+        for (i in 0..6 step 3) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[i + 10] == activePlayer && confirmedMoves[i + 20] == activePlayer) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun diagonalWinner(): Boolean {
+        // check topLeft to bottomRight
+        for (i in 0..2 step 2) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[13] == activePlayer && confirmedMoves[26 - i] == activePlayer) {
+                return true
+            }
+        }
+        // check bottomLeft to topRight
+        for (i in 18..20 step 2) {
+            if (confirmedMoves[i] == activePlayer && confirmedMoves[13] == activePlayer && confirmedMoves[26 - i] == activePlayer) {
+                return true
+            }
+        }
+        return false
     }
 }
