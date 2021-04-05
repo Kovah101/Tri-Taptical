d713196dd4f0a6ee0356dd699485dfa29f926491
Elliot Thompson <elliotthompson101@gmail.com>
2021-04-05 19:05:45 +0100

    Added Login if already created user. Database creates user with display name, ID and Requests Branch

--- app/src/main/java/com/github/kovah101/tri_taptical/Login.kt
+++ app/src/main/java/com/github/kovah101/tri_taptical/Login.kt
@@ -10,12 +10,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
+    // instance of database
+    private val database = Firebase.database
+    private val myRef = database.getReference()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +38,7 @@ class Login : AppCompatActivity() {
         // Check if user is signed in (non-null) and move to menu
         val currentUser = mAuth.currentUser
         if(currentUser != null) {
-            Toast.makeText(applicationContext, "Already logged in", Toast.LENGTH_SHORT).show()
+
             val intent = Intent(this, Menu::class.java)
             intent.putExtra("email", currentUser.email)
             intent.putExtra("userID", currentUser.uid)
@@ -58,7 +62,6 @@ class Login : AppCompatActivity() {
         val user = mAuth.currentUser
         if (user != null) {
             // user already signed in
-            //TODO skip to menu screen
             Toast.makeText(applicationContext, "Already logged in", Toast.LENGTH_SHORT).show()
             loadMenu()
 
@@ -67,7 +70,7 @@ class Login : AppCompatActivity() {
             mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        Toast.makeText(applicationContext, "Successful login", Toast.LENGTH_SHORT)
+                        Toast.makeText(applicationContext, "Successful create user", Toast.LENGTH_SHORT)
                             .show()
                         // set user name
                         val currentUser = mAuth.currentUser
@@ -84,11 +87,27 @@ class Login : AppCompatActivity() {
                                         "Successfully updated display name",
                                         Toast.LENGTH_SHORT
                                     ).show()
+                                    // save to database & create Requests branch
+                                    myRef.child("Users").child(currentUser.displayName!!).setValue(currentUser.uid)
+                                    myRef.child("Users").child(currentUser.displayName!!).child("Requests").setValue(currentUser.email)
                                 }
                             }
+
                         loadMenu()
                     } else {
-                        Toast.makeText(applicationContext, "Failed login", Toast.LENGTH_SHORT)
+                        mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful){
+                                    Toast.makeText(applicationContext, "Successful login", Toast.LENGTH_SHORT)
+                                        .show()
+                                    loadMenu()
+                                } else {
+                                    Toast.makeText(applicationContext, "Failed login - ${task.exception}", Toast.LENGTH_SHORT)
+                                        .show()
+                                    Log.d("Task", "${task.exception}")
+                                }
+                            }
+                        Toast.makeText(applicationContext, "Failed login - ${task.exception}", Toast.LENGTH_SHORT)
                             .show()
                         Log.d("Task", "${task.exception}")
                     }
